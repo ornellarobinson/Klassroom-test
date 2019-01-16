1d98cfa35d1e0abcb0a251b40b247512f0a95a75
Ornella <orazafin@student.42.fr>
2019-01-16 13:15:36 +0100

    Install eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": "airbnb",
+    "plugins": [
+        "react",
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+      "ecmaFeatures": {
+        "jsx": true
+      }
+    },
+}
