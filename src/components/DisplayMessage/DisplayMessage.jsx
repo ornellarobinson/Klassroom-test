7dfc9d53974368cb0588ab0982959d157733ad22
Ornella Robinson <ornella@Ornellas-MacBook-Pro.local>
2019-01-16 21:06:56 +0100

    Create display message component and display messages in Chat page

---
+++ src/components/DisplayMessage/DisplayMessage.jsx
@@ -0,0 +1,32 @@
+// @flow
+
+import React, { PureComponent } from 'react'
+
+const moment = require('moment');
+
+type Props = {
+  message: {
+    from: string,
+    at: Date,
+    message: string
+  }
+}
+
+export default class DisplayMessage extends PureComponent<Props> {
+  render() {
+    const { from, at, message } = this.props.message;
+
+    return (
+      <div className="display-message d-flex align-items-center my-2">
+        <div className="display-message__user-avatar" />
+        <div className="display-message__details ml-3">
+          <div className="details__username-date">
+            <span className="font-weight-bold">{from}</span>
+            <span className="details__date ml-2">{moment(at).format('LT')}</span>
+          </div>
+          {message}
+        </div>
+      </div>
+    )
+  }
+}
--- src/pages/dashboard/chat/Chat.jsx
+++ src/pages/dashboard/chat/Chat.jsx
@@ -1,7 +1,12 @@
 // @flow
 
 import React, { PureComponent } from 'react';
+
 import PostMessage from 'components/PostMessage';
+import DisplayMessage from 'components/DisplayMessage'
+import messages from './Chat.util'
+
+const moment = require('moment');
 
 type Props = {
   type: string
@@ -11,10 +16,15 @@ export default class Chat extends PureComponent<Props> {
   render() {
     const { type } = this.props;
     const name = "general";
-
+  
     return (
       <div className="chat p-4">
-        {type}
+        {moment(messages[0].at).format('LL')}
+        {
+          messages.map(message =>
+            <DisplayMessage message={message}/> 
+          )
+        }
         <PostMessage type={type} name={name} />
       </div>
     )
