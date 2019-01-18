551dd3ead2b243eefab98e83a31cd1329d9d87fe
Ornella <orazafin@student.42.fr>
2019-01-18 01:47:11 +0100

    Add scroll on SearchResultItem container

--- src/redux-files/reducers/userProfile.reducer.js
+++ src/redux-files/reducers/userProfile.reducer.js
@@ -16,7 +16,61 @@ const initialState = [
     username: 'damien',
     name: 'Damien',
     online: true
-  }
+  },
+  {
+    id: 3,
+    username: 'lola',
+    name: 'Lola Dubois',
+    online: true
+  },
+  {
+    id: 4,
+    username: 'John',
+    name: 'John Smith',
+    online: false
+  },
+  {
+    id: 5,
+    username: 'Jane',
+    name: 'Jane Doe',
+    online: true
+  },
+  {
+    id: 6,
+    username: 'Matthieu',
+    name: 'Martin',
+    online: true
+  },
+  {
+    id: 7,
+    username: 'aliciaK',
+    name: 'Alicia Keys',
+    online: false
+  },
+  {
+    id: 8,
+    username: 'stevie',
+    name: 'Stevie',
+    online: true
+  },
+  {
+    id: 9,
+    username: 'ray',
+    name: 'Raymond Carter',
+    online: true
+  },
+  {
+    id: 10,
+    username: 'bob',
+    name: 'bobby smith',
+    online: false
+  },
+  {
+    id: 11,
+    username: 'wdespres',
+    name: 'will despres',
+    online: true
+  },
 ]
 
 export default (state = initialState, action) => {
